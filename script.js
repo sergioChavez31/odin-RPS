c4752e541827be5bbd3cd7c85745d1960d462238
sergMaybeCodes31 <schavez31@gatech.edu>
2025-07-11 17:20:41 -0400

    feat: method to get PC and user choice

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello, world!")
+function getComputerChoice() {
+    let choiceIndex = Math.floor(Math.random() * 3) + 1;
+    let choice;
+    switch (choiceIndex) {
+        case 1:
+            choice = "1, rock";
+            break;
+        case 2:
+            choice = "2, paper";
+            break;
+        case 3:
+            choice = "3, scissors";
+            break;
+        default:
+            choice = "what??"
+    }
+    return choice;
+}
+
+function getHumanChoice() {
+    return prompt("Enter Rock, Paper, or scissors!");
+}
